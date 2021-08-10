25c7259cd838e8c42eef6e28f802e3aaea1ded3d
David Wampamba <davidwampamba@gmail.com>
2021-08-10 10:57:48 +0300

    Process termination programatically

--- session10/app.js
+++ session10/app.js
@@ -1,6 +1,8 @@
+//Start a node server that listens via the http protocol
 const HTTP = require('http')
+
 const HOSTNAME = '127.0.0.1' //localhost
-const PORT = process.env.PORT || 8888
+const PORT = process.env.PORT || 8888 // Use the port from the .env otherwise assign 8888
 
 const SERVER = HTTP.createServer((request,response) => {
     response.statusCode = 200
@@ -11,3 +13,15 @@ const SERVER = HTTP.createServer((request,response) => {
 SERVER.listen(PORT, HOSTNAME,() => {
     console.log(`The server is running at ${HOSTNAME}:${PORT}`)
 })
+
+//How to terminate the node process without using the unsafe CTRL + C (Windows) or Control + C (Mac)
+//Signal Terminate
+
+process.on('SIGTERM', () => {
+  SERVER.close(() => {
+    console.log(`${process.pid} Process terminated`);
+  });
+});
+
+//The process will be terminated after 5 minutes
+setTimeout(()=> process.kill(process.pid, "SIGTERM"), 50000);
