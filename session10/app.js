54dc830221acb02ef9399bfbaabdd1ed27a0e0f6
David Wampamba <davidwampamba@gmail.com>
2021-08-10 11:27:21 +0300

    Working with environment variables

--- session10/app.js
+++ session10/app.js
@@ -1,13 +1,17 @@
 //Start a node server that listens via the http protocol
 const HTTP = require('http')
 
+require('dotenv').config()
+
 const HOSTNAME = '127.0.0.1' //localhost
 const PORT = process.env.PORT || 8888 // Use the port from the .env otherwise assign 8888
 
 const SERVER = HTTP.createServer((request,response) => {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/plain')
-    response.end('Hello World!\n')
+    let { SALT } = process.env
+    
+    response.end(process.env.PUBLIC_KEY)
 })
 
 SERVER.listen(PORT, HOSTNAME,() => {
@@ -24,4 +28,4 @@ process.on('SIGTERM', () => {
 });
 
 //The process will be terminated after 5 minutes
-setTimeout(()=> process.kill(process.pid, "SIGTERM"), 50000);
+setTimeout(()=> process.kill(process.pid, "SIGTERM"), 36000000);
