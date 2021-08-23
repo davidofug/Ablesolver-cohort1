5b668de2a64c187cf46874b599cc0d92a16b886a
David Wampamba <davidwampamba@gmail.com>
2021-08-23 12:17:50 +0300

    Delaying using setTimeout

---
+++ session10/timers.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  // runs after 2 seconds
+    console.log('Ran after 2 seconds')
+}, 2000);
+
+setTimeout(() => {
+  // runs after 50 milliseconds
+    console.log('Ran after 50 milliseconds')
+}, 50);
