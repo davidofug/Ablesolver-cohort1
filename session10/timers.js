d4a40a6ff8437aceda52623396596387760b4bb5
David Wampamba <davidwampamba@gmail.com>
2021-08-23 12:22:50 +0300

    setTimeout with arguments

--- session10/timers.js
+++ session10/timers.js
@@ -1,9 +1,16 @@
-setTimeout(() => {
-  // runs after 2 seconds
-    console.log('Ran after 2 seconds')
-}, 2000);
+// setTimeout(() => {
+//   // runs after 2 seconds
+//     console.log('Ran after 2 seconds')
+// }, 2000);
 
-setTimeout(() => {
-  // runs after 50 milliseconds
-    console.log('Ran after 50 milliseconds')
-}, 50);
+// setTimeout(() => {
+//   // runs after 50 milliseconds
+//     console.log('Ran after 50 milliseconds')
+// }, 50);
+
+//setTimeout with a params
+
+let greeting = (name, msg) => console.log(`${msg} ${name}`)
+
+setTimeout(greeting, 2000, 'David', 'Hola')
+setTimeout(greeting, 500, 'Abudi', 'Agandi')
