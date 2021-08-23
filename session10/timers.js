d3c426f57cfcb6cdd949e15f308df9171a095c4c
David Wampamba <davidwampamba@gmail.com>
2021-08-23 12:38:14 +0300

    setTimeout Id and clearTimeout

--- session10/timers.js
+++ session10/timers.js
@@ -10,7 +10,35 @@
 
 //setTimeout with a params
 
-let greeting = (name, msg) => console.log(`${msg} ${name}`)
+// let greeting = (name, msg) => console.log(`${msg} ${name}`)
 
-setTimeout(greeting, 2000, 'David', 'Hola')
-setTimeout(greeting, 500, 'Abudi', 'Agandi')
+// setTimeout(greeting, 1000, 'David', 'Hola')
+// setTimeout(greeting, 500, 'Abudi', 'Agandi')
+
+//Assign setTimout id to a variable for future use
+//setTimeout ID usecase
+
+const SET_TIME_OUT_ID = setTimeout(() => console.log('Hello Champions!'), 5000)
+
+const INQUIRER = require("inquirer");
+
+const QUESTIONS = [
+  {
+    type: "input",
+    name: "number",
+    message: "Enter any number",
+  }
+];
+
+INQUIRER.prompt(QUESTIONS).then((answers) => {
+  let { number } = answers;
+
+    if (number == 3) {
+        clearTimeout(SET_TIME_OUT_ID)
+        console.log(`setTimeout wouldn't run`)
+    }
+    else
+    {
+        console.log(SET_TIME_OUT_ID)
+    }
+});
