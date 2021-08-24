04f7e1c48178b64713eb7301092472cd751eb619
David Wampamba <davidwampamba@gmail.com>
2021-08-24 11:30:42 +0300

    setInterval and clearInterval download usecase

--- session10/timers2.js
+++ session10/timers2.js
@@ -21,8 +21,60 @@
 // }, 1000);
 
 //To stop the setInterval we utilize the clearInterval by passing the ID of the setInterval()
-const id = setInterval(() => {
-  console.log('The set Interval message')
-}, 2000);
+// const id = setInterval(() => {
+//   console.log('The set Interval message')
+// }, 1000)
 
-clearInterval(id);
+// setTimeout(() => {
+//     clearInterval(id)
+// }, 4000)
+// let count = 0
+// const interval = setInterval(() => {
+//     count++
+//     console.log(count)
+
+//     if (count === 10) {
+//         clearInterval(interval);
+//         // return;
+//     }
+//   // otherwise do things
+// }, 1000);
+
+let payroll = [
+    {
+        "salary": "UGX 15M",
+        "paydate": "25th",
+        "status": false,
+        "name": "David",
+    },
+    {
+        "name": "Jonathan",
+        "salary": "UGX 13M",
+        "paydate": "30th",
+        "status": true
+    },
+    {
+        "name": "Joanita",
+        "salary": "UGX 16M",
+        "paydate": "21st",
+        "status": false
+    }
+]
+
+let index = 0
+const PAYROLL_INTERVAL_ID = setInterval(() => {
+
+    if (payroll[index].status == false) {
+        
+        console.log(`Name: ${payroll[index]['name']} Salary: ${payroll[index].salary} Payment on: ${payroll[index]['paydate']}`)
+        payroll[index]['status'] = true
+    }
+
+    index++;
+
+    if (index === payroll.length) {
+        clearInterval(PAYROLL_INTERVAL_ID)
+        console.log(payroll)
+    }
+
+},1000)
