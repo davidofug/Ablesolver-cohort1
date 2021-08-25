186504041e80fc5ccbfb6076732523f907014332
David Wampamba <davidwampamba@gmail.com>
2021-08-25 11:27:29 +0300

    Returning a promise

--- session10/promises.js
+++ session10/promises.js
@@ -6,7 +6,7 @@
  * 2. Usage of the promise/consuming
 **/
 
-const DONE = false
+/* const DONE = false
 // Definition of the the promise
 const IS_IT_DONE = new Promise((resolve, reject) => {
     const SUCCESS = 'Promise resolved'
@@ -18,14 +18,35 @@ const IS_IT_DONE = new Promise((resolve, reject) => {
       const FAILURE = "Promise failed";
       reject(FAILURE); //Was passed as an argument to the constructor
     }
-})
+}) */
 
 //Usage
-
-const CHECK_IF_DONE = () => {
+/* const CHECK_IF_DONE = () => {
     IS_IT_DONE
     .then(result => console.log(result))
     .catch( error => console.log(error) )
 }
+//Invoke the checker.
+CHECK_IF_DONE() */
 
-CHECK_IF_DONE()
+//Import the file system module 'fs' and assign it to a variable
+const FS = require('fs')
+
+const READ_FILE = (fileName) => {
+    //Create and return a promise 
+    return new Promise((resolve, reject) => {
+        //use the readFile(filename, encoding, callback) method of the fs module
+        FS.readFile(fileName, 'utf8', (err, data) => {
+            if (err) {
+                reject(err) //Reject the promise 
+                return //And stop code execution
+            }
+            resolve(data) //Resolve with data
+        })
+    })
+}
+
+//Use the promise above.
+READ_FILE('./readme.md')
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
