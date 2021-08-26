75a6560bab6186c54b4d1ab40fc69354facd5bf4
David Wampamba <davidwampamba@gmail.com>
2021-08-26 11:33:20 +0300

    ReadFile async/await usecase

--- session10/async-await.js
+++ session10/async-await.js
@@ -12,7 +12,7 @@ const BETTER_PROMISE = async function () {
 
 //2. Chaining Promises in Async/await
 
-const WAIT_FOR_BETTER_PROMISE = async () => {
+/* const WAIT_FOR_BETTER_PROMISE = async () => {
     const HOLD_VALUE_FROM_BETTER_PROMISE = await BETTER_PROMISE()
 
     // if (![null, false, "", undefined].includes(HOLD_VALUE_FROM_BETTER_PROMISE))
@@ -30,3 +30,15 @@ WAIT_FOR_BETTER_PROMISE().then(result => {
     result += '\nGood bye!'
     console.log(result)
 }).catch(error => console.log(error))
+ */
+//Import the file system module 'fs' promises and assign it to a variable
+const FS = require('fs').promises
+
+const READ_FILE = async (fileName) => {
+    let results = await FS.readFile(fileName, 'utf8') // Specify the encoding to get a readable buffer
+    return Buffer.from(results).toString() //Return the Buffer as a string
+}
+
+READ_FILE('readme.md').then(result => {
+    console.log(result)
+}).catch(error => console.error(error))
