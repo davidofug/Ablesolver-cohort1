52138974ead4280722db0a97857cf6dff8fd9fcb
David Wampamba <davidwampamba@gmail.com>
2021-08-26 10:58:11 +0300

    Chaining promises in Async/await

--- session10/async-await.js
+++ session10/async-await.js
@@ -7,6 +7,26 @@ const BETTER_PROMISE = async function () {
 // const BETTER_PROMISE_CONSUMER = BETTER_PROMISE()
 // console.log(BETTER_PROMISE_CONSUMER) will output => Promise { 'Indeed awesome!' }
 
-// To out put the actual string
+// To out put the actual string(value)
 BETTER_PROMISE().then(result => console.log(result))
+
 //2. Chaining Promises in Async/await
+
+const WAIT_FOR_BETTER_PROMISE = async () => {
+    const HOLD_VALUE_FROM_BETTER_PROMISE = await BETTER_PROMISE()
+
+    // if (![null, false, "", undefined].includes(HOLD_VALUE_FROM_BETTER_PROMISE))
+    //     return `${HOLD_VALUE_FROM_BETTER_PROMISE} Async/await does.`;
+    
+    const NOT_ALLOWED = [null, false, "", undefined,[]] //JavaScript false values
+
+    return !NOT_ALLOWED.includes(HOLD_VALUE_FROM_BETTER_PROMISE)
+        ?
+        `${HOLD_VALUE_FROM_BETTER_PROMISE} Async/await does.`
+        : new Error(`Promise didn't resolve`)
+}
+
+WAIT_FOR_BETTER_PROMISE().then(result => {
+    result + '\n Good bye!'
+    console.log(result)
+}).catch(error => console.log(error))
