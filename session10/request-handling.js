b06428b05ccde76dbc2bc08c095a7eb17e9532b6
David Wampamba <davidwampamba@gmail.com>
2021-09-07 10:19:46 +0300

    Making requests

---
+++ session10/request-handling.js
@@ -0,0 +1,17 @@
+const https = require('https')
+const options = {
+    hostname: 'localhost.toplevel',
+    port: 443,
+    path: '/todos',
+    method: 'GET'
+}
+
+const request = https.request(options, (response) => {
+    console.log(`statusCode ${response.statusCode}`)
+
+    response.on('data', returnedData => process.stdout.write(returnedData))
+})
+
+request.on('error', (error) => console.error(error))
+
+request.end()
