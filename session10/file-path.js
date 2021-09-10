0a70544bd78f4715247405054a8c445993024764
David Wampamba <davidwampamba@gmail.com>
2021-09-10 10:33:38 +0300

    File paths

---
+++ session10/file-path.js
@@ -0,0 +1,8 @@
+const path = require('path')
+
+const notes = '/users/joe/notes.txt'
+
+console.log(path.dirname(notes)) // /users/joe
+console.log(path.basename(notes)) //notes.txt
+console.log(path.extname(notes)) //.txt
+console.log(path.basename(notes, path.extname(notes) )) //notes
--- session10/file-stats.js
+++ session10/file-stats.js
@@ -9,7 +9,7 @@
 // //   process.stdout.write(stats)
 // })
 
-const fs = require('fs')
+/* const fs = require('fs')
 try {
   const stats = fs.statSync('test.txt')
   console.log( stats )
@@ -17,4 +17,16 @@ try {
   console.error(err)
 }
 
-console.log('After the stats')
+console.log('After the stats') */
+
+const fs = require('fs')
+
+try {
+    const stats = fs.statSync('test.txt')
+    console.log( `Is it a file? ${stats.isFile()}`)
+    console.log( `Is it a directory? ${stats.isDirectory()}`)
+    console.log( `File size in bytes: ${stats.size}`)
+    console.log( `Is it a symbolic link? ${stats.isSymbolicLink()}`)
+} catch (err) {
+  console.error(err)
+}
