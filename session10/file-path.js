c455cabb94a000f46a1c4933a0a2e18b459aa1e2
David Wampamba <davidwampamba@gmail.com>
2021-09-13 09:44:16 +0300

    Initialize branch

--- session10/file-path.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-const notes = '/users/joe/notes.txt'
-
-console.log(path.dirname(notes)) // /users/joe
-console.log(path.basename(notes)) //notes.txt
-console.log(path.extname(notes)) //.txt
-console.log(path.basename(notes, path.extname(notes) )) //notes
