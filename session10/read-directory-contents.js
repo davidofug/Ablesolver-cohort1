c0473bc949958b5aea7a83095c13938be7c211a9
David Wampamba <davidwampamba@gmail.com>
2021-09-15 10:19:59 +0300

    Code refactor

--- session10/read-directory-contents.js
+++ session10/read-directory-contents.js
@@ -2,15 +2,14 @@ const fs = require('fs')
 const path = require('path')
 const folderPath = 'test'
 
-// const folderPath = 'test/subtest'
-//Read contents of the directory. e.g: Folders and files and see the relative paths
-// try{
-//     const dirContents = fs.readdirSync(folderPath)
-//     console.log(dirContents)
-
-// } catch(err) {
-//     console.error(err)
-// }
+/* const folderPath = 'test/subtest'
+Read contents of the directory. e.g: Folders and files and see the relative paths
+try{
+    const dirContents = fs.readdirSync(folderPath)
+    console.log(dirContents)
+} catch(err) {
+    console.error(err)
+} */
 
 //Read contents of the directory. e.g: Folders and files and see the absolute/full paths
 
