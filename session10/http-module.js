9692a71ada375955fd02078020ed98d88267387e
David Wampamba <davidwampamba@gmail.com>
2021-09-20 10:45:00 +0300

    Info about http module

--- session10/http-module.js
+++ session10/http-module.js
@@ -1,3 +1,7 @@
 const http = require('http')
+//Get information about the http module
+console.log(http)
 
-console.log(http.STATUS_CODES)
+console.log(http.METHODS) //Output all http Methods
+
+console.log(http.STATUS_CODES) //Output all http STATUS_CODES and descriptions
