eee44da77641dbf13fb75c31d35af6c27843fab6
David Wampamba <davidwampamba@gmail.com>
2021-09-21 09:31:58 +0300

    New branch house cleaning

--- session10/http-module.js
+++
@@ -1,7 +0,0 @@
-const http = require('http')
-//Get information about the http module
-console.log(http)
-
-console.log(http.METHODS) //Output all http Methods
-
-console.log(http.STATUS_CODES) //Output all http STATUS_CODES and descriptions
